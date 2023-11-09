d4285bce46153f495580fb7d07331904f010d452
CarryUa <141311142+CarryUa@users.noreply.github.com>
2023-11-09 16:57:13 +0200

    Add files via upload

---
+++ Aftopia/aftopia/script.js
@@ -0,0 +1,48 @@
+const button = document.querySelector(".header__login");
+button.addEventListener("click", () => {
+  button.classList.toggle("active");
+})
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// ==========================================================
+
+
+function firstToUpperCase(str)
+{
+let arr=str.split("")
+let i=0
+while(i != arr.length)
+{
+  arr[0]=arr[0].toUpperCase()
+  if(arr[i-1] == " "){
+    arr[i]=arr[i].toUpperCase()
+  }
+i++
+}
+return arr.join("")
+}
+
+let str = "here is your string"
+console.log("str before func: ", str)
+str = firstToUpperCase(str)
+console.log("str after func: ", str)
