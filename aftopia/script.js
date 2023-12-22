658ecedba1b0f9914acded6bdbe378fa41a64822
CarryUa <141311142+CarryUa@users.noreply.github.com>
2023-12-22 18:47:42 +0200

    Added footer and media request

--- aftopia/script.js
+++ aftopia/script.js
@@ -3,64 +3,24 @@ button.addEventListener("click", () => {
   button.classList.toggle("active");
   button.innerHTML = "Singed in";
 });
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-
-// ==========================================================
-
-// function firstToUpperCase(str)
-// {
-// let arr=str.split("")
-// let i=0
-// while(i != arr.length)
-// {
-//   arr[0]=arr[0].toUpperCase()
-//   if(arr[i-1] == " "){
-//     arr[i]=arr[i].toUpperCase()
-//   }
-// i++
-// }
-// return arr.join("")
-// }
-
-// let str = "here is your string"
-// console.log("str before func: ", str)
-// str = firstToUpperCase(str)
-// console.log("str after func: ", str)
-
-// ==========================================================
-
-// ==========================================================
-// let nScroll;
-// window.addEventListener("scroll", () => {
-//   nScroll = window.scrollY;
-//   if (nScroll > 500) {
-//     console.log(nScroll + "px");
-//   }
-//   document.querySelector("#scroll__text").innerHTML = nScroll + "px";
-// });
 const button2 = document.querySelector("#sub2getnews__input-submit");
-console.log(button2);
+
 button2.addEventListener("click", () => {
   button2.classList.toggle("done");
   button2.innerHTML = "Done";
+});
+
+const loginForm = document.querySelector(".login");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (e.target.email.value != 0) {
+    loginForm.classList.add("logged");
+    main.classList.remove("hidden");
+    header.classList.remove("hidden");
+  } else {
+    console.log("sus");
+  }
 });
